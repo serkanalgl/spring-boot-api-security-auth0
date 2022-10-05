40dabe7a1fb9b8b1f84b15b0feb864ec7f35b506
serkanalgl <serkanalgl@gmail.com>
2022-10-05 15:48:40 +0300

    spring security oauth2 implemented messages endpoint for public, private, private with has authority

---
+++ src/main/java/org/github/serkanalgl/api/security/auth0/controller/MessageController.java
@@ -0,0 +1,33 @@
+package org.github.serkanalgl.api.security.auth0.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.github.serkanalgl.api.security.auth0.dto.MessageDTO;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/messages", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class MessageController {
+
+    @GetMapping(value = "/public")
+    public MessageDTO publicEndpoint() {
+        return new MessageDTO("All good. You DO NOT need to be authenticated to call /messages/public");
+    }
+
+    @GetMapping(value = "/private")
+    public MessageDTO privateEndpoint() {
+        return new MessageDTO("All good. You can see this because you are Authenticated.");
+    }
+
+    @GetMapping(value = "/private-scoped")
+    @PreAuthorize("hasAuthority('SCOPE_read:messages')")
+    public MessageDTO privateScopedEndpoint() {
+        return new MessageDTO("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
+    }
+
+}
---
+++ src/main/java/org/github/serkanalgl/api/security/auth0/dto/MessageDTO.java
@@ -0,0 +1,12 @@
+package org.github.serkanalgl.api.security.auth0.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageDTO {
+
+    private String message;
+
+}
